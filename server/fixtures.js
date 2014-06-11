12c42bde57a82bdc23ce3e01741e066059ca66d8
phillipkregg <phillipkregg@gmail.com>
2014-06-11 21:50:45 +0000

    Removed autopublish and setup Posts publication

---
+++ server/fixtures.js
@@ -0,0 +1,30 @@
+if (Posts.find().count() === 0) {
+  Posts.insert({
+    title: 'Introducing Telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  });
+  
+  Posts.insert({
+    title: 'Meteor',
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+  });
+  
+  Posts.insert({
+    title: 'The Meteor Book',
+    author: 'Tom Coleman',
+    url: 'http://themeteorbook.com'
+  });
+  
+  Posts.insert({
+    title: 'Reddit',
+    author: 'Phillip',
+    url: 'http://www.reddit.com/'
+  });
+  
+}
+
+
+
+
