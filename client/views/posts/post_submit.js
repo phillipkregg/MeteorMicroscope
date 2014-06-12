4fda880a481790f774bca7a7c23510a577ef57d6
phillipkregg <phillipkregg@gmail.com>
2014-06-12 16:22:56 +0000

    Removed the insecure package and allowed only logged in users to make new posts

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -4,13 +4,15 @@ Template.postSubmit.events({
     e.preventDefault();
     
      var post = {
-       url: $(e.target).find('[name=title]').val(),
+       url: $(e.target).find('[name=url]').val(),
        title: $(e.target).find('[name=title]').val(),
        message: $(e.target).find('[name=message]').val()
      };
     
     // The insert function automatically returns the _id of the new post
     post._id = Posts.insert(post);
+    
+    // The router uses the post _id - just generated - to know where to go
     Router.go('postPage', post);
     
   }
