b209a0ea8c27c70993495f6b0b5f483d039a0ba9
phillipkregg <phillipkregg@gmail.com>
2014-06-12 16:34:59 +0000

    Added access denied page for when user is not logged in

--- collections/posts.js
+++ collections/posts.js
@@ -1,6 +1,7 @@
 Posts = new Meteor.Collection('posts');
 
 Posts.allow({
+  // The userId is always passed to the allow and deny methods
   insert: function(userId, doc) {
     // only allow posting if you are logged in
     return !! userId;
